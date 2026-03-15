5eafc6f2b530e71bcbca002ee8b7a31a1e76d8bd
nidhi2609 <nidhirahangdale2@gmail.com>
2026-03-15 08:00:00 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
 const server = express();
+server.use('/hello/2',(req,res)=>{
+  res.send("hello 2")
+})
+
+server.use("/hello",(req,res)=>{
+  res.send("Hi helooo")
+})
 
 server.use('/test',(req,res)=>{
-  res.send("Hi it's me");
+  res.send("Hi testingggg");
+})
+
+
+
+server.use('/',(req,res)=>{
+  res.send("Wildcard")
 })
 
 server.listen(7777,()=>{
