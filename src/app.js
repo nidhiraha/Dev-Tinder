d897dc0e5f2fd3ccaee3537068cfcd371aaa2d20
nidhi2609 <nidhirahangdale2@gmail.com>
2026-03-15 08:21:02 +0530

    router param, query

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 const express = require('express');
 
 const server = express();
-server.use('/hello/2',(req,res)=>{
-  res.send("hello 2")
-})
 
-server.use("/hello",(req,res)=>{
-  res.send("Hi helooo")
-})
+server.use('/user', (req, res) => {
+  console.log(req.query.name,req.query.id);
+  res.send("Advance routing technique!!");
+});``
 
-server.use('/test',(req,res)=>{
-  res.send("Hi testingggg");
-})
-
-
-
-server.use('/',(req,res)=>{
-  res.send("Wildcard")
-})
-
-server.listen(7777,()=>{
-  console.log("Server running at port 7777...")
+server.listen(7777, () => {
+  console.log("Server running at port 7777...");
 });
